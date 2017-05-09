b84da18b83bea43a2a82c31256db45cdfc44281a
knight-shade <abhishekpoonia22@gmail.com>
2017-05-09 18:59:49 +0530

    Add map in user and org

--- app/src/main/java/com/samkeet/takeup/organisation/OrgActivity.java
+++ app/src/main/java/com/samkeet/takeup/organisation/OrgActivity.java
@@ -1,15 +1,31 @@
 package com.samkeet.takeup.organisation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.samkeet.takeup.R;
 
-public class OrgActivity extends AppCompatActivity {
+public class OrgActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_org);
+        setContentView(R.layout.activity_user);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).draggable(false).title("Map marker"));
+
     }
 }
