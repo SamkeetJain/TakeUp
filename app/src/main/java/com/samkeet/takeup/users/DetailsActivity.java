ca5e3de5bb7e08658b87da892d81f7e6d6f38b87
knight-shade <abhishekpoonia22@gmail.com>
2017-05-10 06:52:48 +0530

    User and Org. details calls added

--- app/src/main/java/com/samkeet/takeup/users/DetailsActivity.java
+++ app/src/main/java/com/samkeet/takeup/users/DetailsActivity.java
@@ -1,7 +1,9 @@
 package com.samkeet.takeup.users;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.samkeet.takeup.R;
 
@@ -10,6 +12,8 @@ import org.json.JSONException;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    private TextView mPlantName,mPlantDetails,mPlantTakeUpStatus,mPlantTakeUpUser,mUserAddedName;
+    private ImageView mPlantImage;
     public JSONArray mTreeObject;
 
     @Override
@@ -22,5 +26,11 @@ public class DetailsActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        mPlantName = (TextView) findViewById(R.id.plant_name);
+        mPlantDetails = (TextView) findViewById(R.id.plant_details);
+        mPlantTakeUpStatus = (TextView) findViewById(R.id.plant_take_up_status);
+        mPlantTakeUpUser = (TextView) findViewById(R.id.plant_take_up_user);
+        mPlantImage = (ImageView) findViewById(R.id.plant_image);
     }
 }
